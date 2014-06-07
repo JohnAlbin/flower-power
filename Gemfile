bf7262fb5fc274e113fc77025d9a6adf079f7024
JohnAlbin <virtually.johnalbin@gmail.com>
2014-06-07 17:12:47 -0500

    Add Gemfile and config.rb.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'sass',     '~> 3.3.x'
+gem 'compass',  '>= 1.0.0.alpha.19'
---
+++ config.rb
@@ -0,0 +1,20 @@
+# Location of the project's resources.
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "images"
+javascripts_dir = "js"
+fonts_dir       = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style    = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments   = false
+
+# To enable sourcemap support. Uncomment:
+sourcemap       = true
